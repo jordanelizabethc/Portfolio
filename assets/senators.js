efa770d2a20f907bddf1c00dbc01a1bdb0fa3d7b
jordanececil <10626023@my.uvu.edu>
2018-10-30 12:47:50 -0600

    sos with senator js changes

--- assets/senators.js
+++ assets/senators.js
@@ -1,4 +1,4 @@
-{
+export const senatorData = {
     "status": "OK",
     "copyright": " Copyright (c) 2018 Pro Publica Inc. All Rights Reserved.",
     "results": [{
--- main.js
+++ main.js
@@ -31,3 +31,26 @@ limitedPeople.forEach((person) => {
     peopleList.appendChild(listItem)
 })
 document.appendChild("<br>")
+
+import { senatorData } from './assets/senators.js'
+
+
+// const senators = 
+console.log(senatorData.results[0].members)
+
+
+const republicans = senators.filter(senator => senator.party === "R")
+
+const democrats = senators.filter(senators => senator.party === "D")
+
+const females = senators.filter(senator => senator.gender === 'F')
+
+const males = senators.filter(senator => senator.gender === 'M')
+
+const loyalRepublican = republicans.reduce((acc, senator) => senator.votes_with_party_pct > 0 ? senator : acc, 0)
+
+//const loyalDemocrat = 
+
+console.log('There are ${republicans.length} republicans and ${democrats.length} democrats in the Senate')
+
+console.log('The most loyal Republican is ${loyalRepublican.first_name} ${loyalRepublican.last_name} from ${loyalRepublican.state} who votes with republicans ${loyalRepublican.votes_with_party_pct}% of the time.')
