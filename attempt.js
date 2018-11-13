06edc68a4c768c3ded9a0d18d93d40761d58f5c4
jordanececil <10626023@my.uvu.edu>
2018-11-13 14:02:34 -0700

    accessed correct javascript file

--- attempt.js
+++ attempt.js
@@ -1,6 +1,5 @@
 import { people } from '/assets/people.js';
 
-// document.getElementById('card1').innerHTML = people[0].name + "<br>" + people[0].height + "<br>" + people[0].mass + "<br>" + people[0].hair_color + "<br>" + people[0].skin_color + "<br>" + people[0].birth_year + "<br>" + people[0].gender
 
 let sceneDiv = document.querySelector('.scene')
 
@@ -40,35 +39,6 @@ const cardCreator = (person) => {
 
 people.forEach(element => cardCreator(element))
 
-
-// var card = document.querySelector('.card');
-// card.addEventListener('click', function() {
-//     card.classList.toggle('is-flipped');
-// });
-
-{
-    /* <div class="card">
-    <div class="card__face card__face--front"><img class="imagesize" src="https://vignette.wikia.nocookie.net/starwars/images/d/d9/Luke-rotjpromo.jpg/revision/latest?cb=20091030151422">
-    </div>
-    <div class="card__face card__face--back" id="card1"></div>
-    </div> */
-}
-
-// let limitedPeople = people.slice(0, 1)
-// let peopleList = document.querySelector("#card1")
-
-// limitedPeople.forEach((person) => {
-//     let listItem = document.createElement('li')
-//     listItem.textContent = person.name
-//     peopleList.appendChild(listItem)
-// })
-
-// limitedPeople.forEach((person) => {
-//     let listItem = document.createElement('li')
-//     listItem.textContent = person.height
-//     peopleList.appendChild(listItem)
-// })
-
 let newCard = {
     "name": "Thor Anderson",
     "height": "200",
@@ -80,15 +50,3 @@ let createCard = document.querySelector('#createCard')
 createCard.addEventListener('click', () => {
     return cardCreator(newCard)
 })
-
-// var createCard = document.getElementById('createCard')
-// createCard.addEventListener('click', function() {
-//     var container = document.getElementById('main');
-//     container.innerHTML +=
-//         '<div class="scene scene--card">' +
-//         '<div class="card">' +
-//         '<div class="card__face card__face--front">' + '<img src="' + document.getElementsByName('cardImage')[0].value + '">' +
-//         '</div>' +
-//         '<div class="card__face card__face--back">' + document.getElementsByName('cardName')[0].value + '<br>' + document.getElementsByName('cardStats')[0].value + '</div>' +
-//         '</div>'
-// })
