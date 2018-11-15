8a8234629f069e883476a2481c168c9dec855458
Thor Anderson <ta.anderson@gmail.com>
2018-11-15 12:05:40 -0700

    Cleaning up star wars images

--- star-wars-guide/app/components/header/headerController.js
+++
@@ -1,6 +0,0 @@
-angular.module('StarWarsApp')
-	.controller('headerController', ['$scope', 'LxDialogService', function($scope, LxDialogService){
-		$scope.openDialog = function(dialogId){
-			LxDialogService.open(dialogId);
-		};
-	}]);
