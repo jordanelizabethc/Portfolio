daac053e5230511475c4162a04bcf3ea0c22ac1c
jordanececil <10626023@my.uvu.edu>
2018-11-30 23:02:14 -0700

    hi git scares me

---
+++ attempt.js
@@ -0,0 +1,59 @@
+import { people } from '/assets/people.js';
+
+
+let sceneDiv = document.querySelector('.scene')
+
+const cardCreator = (person) => {
+    let cardDiv = document.createElement('div')
+    cardDiv.addEventListener('click', function() {
+        cardDiv.classList.toggle('is-flipped');
+    })
+    let frontCard = document.createElement('div')
+    let backCard = document.createElement('div')
+
+    let name = document.createElement('p')
+    let height = document.createElement('p')
+    let mass = document.createElement('p')
+    let birth_year = document.createElement('p')
+    let gender = document.createElement('p')
+    name.textContent = `Name: ${person.name}`
+    height.textContent = `Height: ${person.height}`
+    mass.textContent = `Mass: ${person.mass}`
+    birth_year.textContent = `Birth Year: ${person.birth_year}`
+    gender.textContent = `Gender: ${person.gender}`
+    backCard.appendChild(name)
+    backCard.appendChild(height)
+    backCard.appendChild(mass)
+    backCard.appendChild(birth_year)
+    backCard.appendChild(gender)
+    cardDiv.className = "card"
+    frontCard.className = "card__face card__face--front"
+    backCard.className = "card__face card__face--back"
+    let frontImg = document.createElement('img')
+    let urlEnd = person.url.slice(person.url.length - 3)
+    let characterNumber = urlEnd.replace(/\//gi, '')
+    frontImg.src = `./characters/${characterNumber}.jpg`
+    frontCard.appendChild(frontImg)
+
+
+    // now, add the front and back cards to the containing div
+    cardDiv.appendChild(frontCard)
+    cardDiv.appendChild(backCard)
+    sceneDiv.appendChild(cardDiv)
+    console.log(person.name)
+};
+
+people.forEach(element => cardCreator(element))
+
+let newCard = {
+    "name": "Jordan Cecil",
+    "height": "71",
+    "mass": "70",
+    "birth_year": "1995",
+    "gender": "female",
+}
+
+let createCard = document.querySelector('#createCard')
+createCard.addEventListener('click', () => {
+    return cardCreator(newCard)
+})
