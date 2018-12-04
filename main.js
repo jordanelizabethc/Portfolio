20e64b232b88586c40f61c4e594ac16ff5c6ce66
jordanececil <10626023@my.uvu.edu>
2018-12-04 11:57:44 -0700

    Created JS file for senator HTML

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 import { films } from '/assets/films.js'
 import { people } from '/assets/people.js'
-import { senatorData } from './assets/senators.js'
 
-// People Info
+
+// FILM LIST
 let filmList = document.querySelector("#film-list")
 
 films.forEach((film) => {
@@ -12,6 +12,7 @@ films.forEach((film) => {
     console.log(film.title)
 })
 
+// PEOPLE LIST
 let limitedPeople = people.slice(0, 7)
 
 let peopleList = document.querySelector("#people-list")
@@ -20,46 +21,4 @@ limitedPeople.forEach((person) => {
     let listItem = document.createElement('li')
     listItem.textContent = person.name
     peopleList.appendChild(listItem)
-})
-
-// Senator Info
-const senators = senatorData.results[0].members
-
-console.log(senators)
-
-
-const republicans = senators.filter(senator => senator.party === "R")
-const democrats = senators.filter(senator => senator.party === "D")
-
-const maleSenators = senators.filter(senator => senator.gender === 'M')
-
-const femaleSenators = senators.filter(senator => senator.gender === 'F')
-
-const mostLoyalRepublican = republicans.reduce((acc, senator) => senator.votes_with_party_pct > 0 ? senator : acc, 0)
-
-const mostLoyalDemocrat = democrats.reduce((acc, senator) => senator.votes_with_party_pct > 0 ? senator : acc, 0)
-
-console.log('The senator who is the most loyal to the republican party is $ {mostLoyalRepublican.first_name} ${mostLoyalRepublican.last_name} from {mostLoyalRepublican.state} who votes with republicans ${mostLoyalRepublican.votes_with_party_pct}% of the time')
-
-console.log('The senator who is the most loyal to the democratic party is $ {mostLoyalDemocrat.first_name} ${mostLoyalDemocrat.last_name} from {mostLoyalDemocrat.state} who votes with republicans ${mostLoyalDemocrat.votes_with_party_pct}% of the time')
-
-
-console.log('There are ${republicans.length} republicans and ${democrats.length} democrats in the Senate')
-
-const senWithPics = senators.map(senator => {
-    senator.imgURL = 'https://someurl/${senator.govtrack_id}-200px.jpeg'
-    return senator
-})
-console.log(senWithPics)
-let pictureDiv = document.querySelector('.container')
-
-senWithPics.forEach(senator => {
-    let senatorPic = document.createElement('img')
-    let senatorFig = document.createElement('figure')
-    let senatorCap = document.createElement('figcaption')
-    senatorCap.textContent = '${senator.first_name} ${senator.last_name}'
-    senatorPic.src = senator.imgURL
-    senatorFig.appendChild(senatorFig)
-    senatorFig.appendChild(senatorCap)
-    pictureDiv.appendChild(senatorPic)
 })
---
+++ senator.js
@@ -0,0 +1,43 @@
+import { senatorData } from './assets/senators.js'
+
+// Senator Info
+const senators = senatorData.results[0].members
+
+console.log(senators)
+
+
+const republicans = senators.filter(senator => senator.party === "R")
+const democrats = senators.filter(senator => senator.party === "D")
+
+const maleSenators = senators.filter(senator => senator.gender === 'M')
+
+const femaleSenators = senators.filter(senator => senator.gender === 'F')
+
+const mostLoyalRepublican = republicans.reduce((acc, senator) => senator.votes_with_party_pct > 0 ? senator : acc, 0)
+
+const mostLoyalDemocrat = democrats.reduce((acc, senator) => senator.votes_with_party_pct > 0 ? senator : acc, 0)
+
+console.log('The senator who is the most loyal to the republican party is $ {mostLoyalRepublican.first_name} ${mostLoyalRepublican.last_name} from {mostLoyalRepublican.state} who votes with republicans ${mostLoyalRepublican.votes_with_party_pct}% of the time')
+
+console.log('The senator who is the most loyal to the democratic party is $ {mostLoyalDemocrat.first_name} ${mostLoyalDemocrat.last_name} from {mostLoyalDemocrat.state} who votes with republicans ${mostLoyalDemocrat.votes_with_party_pct}% of the time')
+
+
+console.log('There are ${republicans.length} republicans and ${democrats.length} democrats in the Senate')
+
+const senWithPics = senators.map(senator => {
+    senator.imgURL = 'https://someurl/${senator.govtrack_id}-200px.jpeg'
+    return senator
+})
+console.log(senWithPics)
+let pictureDiv = document.querySelector('.container')
+
+senWithPics.forEach(senator => {
+    let senatorPic = document.createElement('img')
+    let senatorFig = document.createElement('figure')
+    let senatorCap = document.createElement('figcaption')
+    senatorCap.textContent = '${senator.first_name} ${senator.last_name}'
+    senatorPic.src = senator.imgURL
+    senatorFig.appendChild(senatorFig)
+    senatorFig.appendChild(senatorCap)
+    pictureDiv.appendChild(senatorPic)
+})
