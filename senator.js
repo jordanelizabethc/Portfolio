843b3ef8deb94eeed42dbdd1550a0a488f0ee4f9
jordanececil <10626023@my.uvu.edu>
2018-12-12 15:46:07 -0700

    CARDS WORKED

--- senator.js
+++ senator.js
@@ -1,40 +1,40 @@
 import { senatorData } from './assets/senators.js'
 
-// let pictureDiv = document.querySelector('.container')
+
 
 // Senator Info
 
 const senators = senatorData[0].results[0].members
 
-console.log(senators)
+// console.log(senators)
 
-const republicans = senators.filter(senator => senator.party === "R")
-const democrats = senators.filter(senator => senator.party === "D")
+// const republicans = senators.filter(senator => senator.party === "R")
+// const democrats = senators.filter(senator => senator.party === "D")
 
-const maleSenators = senators.filter(senator => senator.gender === 'M')
+// const maleSenators = senators.filter(senator => senator.gender === 'M')
 
-const femaleSenators = senators.filter(senator => senator.gender === 'F')
+// const femaleSenators = senators.filter(senator => senator.gender === 'F')
 
-const mostLoyalRepublican = republicans.reduce((acc, senator) => senator.votes_with_party_pct > 0 ? senator : acc, 0)
+// const mostLoyalRepublican = republicans.reduce((acc, senator) => senator.votes_with_party_pct > 0 ? senator : acc, 0)
 
-const mostLoyalDemocrat = democrats.reduce((acc, senator) => senator.votes_with_party_pct > 0 ? senator : acc, 0)
+// const mostLoyalDemocrat = democrats.reduce((acc, senator) => senator.votes_with_party_pct > 0 ? senator : acc, 0)
 
-console.log(`The senator who is the most loyal to the republican party is $ ${mostLoyalRepublican.first_name} ${mostLoyalRepublican.last_name} from ${mostLoyalRepublican.state} who votes with republicans ${mostLoyalRepublican.votes_with_party_pct}% of the time`)
+// console.log(`The senator who is the most loyal to the republican party is $ ${mostLoyalRepublican.first_name} ${mostLoyalRepublican.last_name} from ${mostLoyalRepublican.state} who votes with republicans ${mostLoyalRepublican.votes_with_party_pct}% of the time`)
 
-console.log(`The senator who is the most loyal to the democratic party is $ ${mostLoyalDemocrat.first_name} ${mostLoyalDemocrat.last_name} from ${mostLoyalDemocrat.state} who votes with republicans ${mostLoyalDemocrat.votes_with_party_pct}% of the time`)
+// console.log(`The senator who is the most loyal to the democratic party is $ ${mostLoyalDemocrat.first_name} ${mostLoyalDemocrat.last_name} from ${mostLoyalDemocrat.state} who votes with republicans ${mostLoyalDemocrat.votes_with_party_pct}% of the time`)
 
 
-console.log(`There are ${republicans.length} republicans and ${democrats.length} democrats in the Senate`)
-console.log(maleSenators)
-console.log(femaleSenators)
+// console.log(`There are ${republicans.length} republicans and ${democrats.length} democrats in the Senate`)
+// console.log(maleSenators)
+// console.log(femaleSenators)
 
-// SENATOR PICTURES
+// SENATOR CARD
 
 const senWithPics = senators.map(senator => {
-        senator.imgURL = `https://www.govtrack.us/data/photos/${senator.govtrack_id}-200px.jpeg`
-        return senator
-    })
-    // console.log(senWithPics)
+    senator.imgURL = `https://www.govtrack.us/data/photos/${senator.govtrack_id}-200px.jpeg`
+    return senator
+})
+
 
 let pictureDiv = document.querySelector('.scene')
 
@@ -70,60 +70,6 @@ senWithPics.forEach(senator => {
 
 });
 
-//CREATE SENATOR CARD
-
-
-
-
-
-
-
-
-
-
-// let createElement = document.createElement('p')
-// createElement.textContent = "hi"
-// parentnode.appendChild(createElement)
-// function CardCreator(senatemember) {
-
-// }
-
-// const cardCreator = (senators) => {
-//     //delete old cards
-//     while (pictureDiv.firstChild)(
-//         pictureDiv.removeChild(pictureDiv.firstChild)
-//     )
-
-//     let cardDiv = document.createElement('div')
-//     cardDiv.addEventListener('click', function() {
-//         cardDiv.classList.toggle('is-flipped');
-//     })
-//     let frontCard = document.createElement('div')
-//     let backCard = document.createElement('div')
-//     let firstname = document.createElement('p')
-//     firstname.textContent = `Name: senators ${first_name}`
-//     height.textContent = `Height: ${person.height}`
-//     mass.textContent = `Mass: ${person.mass}`
-//     birth_year.textContent = `Birth Year: ${person.birth_year}`
-//     gender.textContent = `Gender: ${person.gender}`
-//     backCard.appendChild(name)
-//     backCard.appendChild(height)
-//     backCard.appendChild(mass)
-//     backCard.appendChild(birth_year)
-//     backCard.appendChild(gender)
-//     cardDiv.className = "card"
-//     frontCard.className = "card__face card__face--front"
-//     backCard.className = "card__face card__face--back"
-//     let frontImg = document.createElement('img')
-//     frontImg.src = `https://www.govtrack.us/data/photos/${senator.govtrack_id}-200px.jpeg`
-//     frontCard.appendChild(frontImg)
-//         //Sort by Party
-//     let filteredByParty = senators.filter(senator => senator.party === party)
-
-//     filteredByParty.forEach(senator => {
-//         createCard(senator)
-//     })
-// }
 
 
 //EXAMPLE HP CODE
